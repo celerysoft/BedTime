1aa135805787787f5c4ca7a70e7a1d1a8a916a85
Celery Qin <celerysoft@gmail.com>
2016-09-22 12:02:28 +0800

    update gradle to 2.2.0

---
+++ app/src/main/java/com/celerysoft/bedtime/receiver/OnNotificationClickReceiver.java
@@ -0,0 +1,17 @@
+package com.celerysoft.bedtime.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by admin on 16/9/21.
+ *
+ */
+
+public class OnNotificationClickReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
